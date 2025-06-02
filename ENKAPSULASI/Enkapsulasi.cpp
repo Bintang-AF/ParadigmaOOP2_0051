0d2f09f25a68205c85ccbcb476dd7928855de954
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-06-02 10:05:35 +0700

    membuat class serta modifiernya

--- ENKAPSULASI/Enkapsulasi.cpp
+++ ENKAPSULASI/Enkapsulasi.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 using namespace std;
 
+class remotelampu {
+    private :
+    string saklarNo[10];
+    public :
+    void setSaklarNo(int i, string value){
+        saklarNo [i] = value ;
+        
+    }
+}
