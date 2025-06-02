b1f14b4a551a23f66bcd1bcde0056acf6c34346d
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-06-02 10:00:49 +0700

    menampilkan main yg telah dibuat sebelumnya

--- ABSTRAKSI/Absatraksi.cpp
+++ ABSTRAKSI/Absatraksi.cpp
@@ -17,3 +17,11 @@ class AbstraksiKlas {
         cout << "y = "<< y << endl;
     }
 };
+
+int main (){
+    AbstraksiKlas ak;
+    ak.setXY("yogyakarta", "kampus");
+    ak.display();
+
+    return 0;
+}
